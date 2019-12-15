c306c5d8bf37105f5f6d7a4adf8fa8717b0268be
Ziyang Men <791191656@qq.com>
2019-12-15 21:15:08 +0800

    12.15 数据结构上机作业2

--- homework_2.cpp
+++ homework_2.cpp
@@ -1,9 +1,12 @@
+/*
+    整个c++文件在vscode2019下用gdb32编译器进行编译，无报错
+    门子扬, 2016200103038, 格拉斯哥学院
+*/
+#include <string.h>
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
-#include <queue>
-
-#include "stack.h"
 using namespace std;
 const int maxn = 1e5 + 5;  //车辆编号的最大值
 
@@ -41,7 +44,7 @@ void stack_pop(Stack S) {
         printf("please create stack\n");
         return;
     }
-    if (!isEmpty(S)) {
+    if (!stack_isEmpty(S)) {
         ptrToNode ptr = S->next;
         S->next = ptr->next;
         free(ptr);
@@ -85,20 +88,9 @@ Element stack_top(Stack S) {
         printf("please create stack\n");
         return -1;
     }
-    if (!isEmpty(S)) return S->next->data;
+    if (!stack_isEmpty(S)) return S->next->data;
     printf("the stack is empty\n");
     return 0;
-}
-
-//销毁栈
-void destoryStack(Stack &S) {
-    if (S != NULL) {
-        makeEmpty(S);
-        ptrToNode ptr = S;
-        free(ptr);
-        S = NULL;
-        length = 0;
-    }
 }
 
 Element stack_size(Stack S) {
@@ -110,29 +102,101 @@ Element stack_size(Stack S) {
 }
 /*********************END STACK***********************/
 
+/***********************QUEUE*************************/
+//定义节点
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+//定义队列（保存队首和队尾指针）
+typedef struct queue_link {
+    node *front;
+    node *rear;
+} que;
+
+//初始化队列
+que *InitQueue() {
+    que *q = (que *)malloc(sizeof(que));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+//判断队列是否为空
+int EmptyQueue(que *q) { return q->front == NULL; }
+
+//入队
+void InQueue(que *q, char data) {
+    node *n = (node *)malloc(sizeof(node));
+    if (n == NULL)  //内存分配失败
+        return;
+    n->data = data;
+    n->next = NULL;  //尾插法，插入元素指向空
+    if (q->rear == NULL) {
+        q->front = n;
+        q->rear = n;
+    } else {
+        q->rear->next = n;  //让n成为当前的尾部节点下一节点
+        q->rear = n;        //尾部指针指向n
+    }
+}
+
+//出队(删除队首元素)
+void DeQueue(que *q) {
+    node *n = q->front;
+    if (q->front == NULL)  //判断队列是否为空
+        return;
+    if (q->front == q->rear)  //是否只有一个元素
+    {
+        q->front = NULL;
+        q->rear = NULL;
+    } else {
+        q->front = q->front->next;
+        free(n);
+    }
+}
+
+//打印队列
+void DisplayQueue(que *q) {
+    node *n = (node *)malloc(sizeof(node));
+    n = q->front;
+    if (n == NULL) {
+        return;  //队列为空
+    }
+    while (n != NULL) {
+        printf("%d ", n->data);
+        n = n->next;
+    }
+    printf("\n");
+}
+/**********************END QUEUE*************************/
+
 Stack park, tmp_park;
-queue<int> wait;
+que *wait;
 int n, price;
-int T;                   // Time
-int car_state[maxn][2];  //[i][0]代表该辆车进入停车场的时间
+int car_state[maxn][3];  //[i][0]代表该辆车进入停车场的时间
 //[i][1]代表该辆车处在哪个位置，0为park，1为tmp，2为wait
 //[i][2]代表这个车在停车场时，由于其他车的出去而离开停车场的时间
 
+//有车进入
 void car_in(int id, int time) {
     if (stack_size(park) < n) {
         car_state[id][0] = time;
         car_state[id][1] = 0;
+        car_state[id][2] = 0;
         stack_push(park, id);
     } else {
         car_state[id][0] = -1;
         car_state[id][1] = 2;
-        wait.push(id);
+        car_state[id][2] = 0;
+        InQueue(wait, id);
     }
     return;
 }
 
+//查询操作
 void query(int id, int now) {
-    cout << "NO. " << id << " is in: ";
+    cout << "Car NO. " << id << " is in: ";
     if (car_state[id][1] == 0) {
         cout << "Park area" << endl;
     } else if (car_state[id][1] == 1) {
@@ -140,7 +204,11 @@ void query(int id, int now) {
     } else {
         cout << "Waiting area" << endl;
     }
-    cout << "NO. " << id << " needs to be charged: $";
+
+    cout << "it spent " << car_state[id][2]
+         << " unit time due to other car leave the park." << endl;
+
+    cout << "Car NO. " << id << " needs to be charged: $";
     if (car_state[id][0] < 0) {
         cout << 0 << endl;
     } else {
@@ -149,53 +217,86 @@ void query(int id, int now) {
     return;
 }
 
-int car_leave(int id) {
+//车离开
+//每辆车进出都要花费单位1的时间
+int car_leave(int id, int time) {
+    int T = time;
     if (car_state[id][1] == 0) {  //在停车场
-        while (stack_top(park) != id) {
+        while (stack_top(park) != id) {  //将离开车后面的车先推出到临时区域
             stack_push(tmp_park, stack_top(park));
+            car_state[stack_top(park)][1] = 1;
             car_state[stack_top(park)][2] -= T++;
             stack_pop(park);
         }
+
+        cout << "Car NO. " << stack_top(park)  //离开
+             << " will leave the park area, need to be charged: "
+             << (T - car_state[id][0] - car_state[id][2]) * price << endl;
+        cout << "The time it leave is: " << T << endl;
         stack_pop(park);
-        while (!stack_isEmpty(tmp_park)) {
+
+        while (!stack_isEmpty(tmp_park)) {  //临时区域的车回来
             stack_push(park, stack_top(tmp_park));
+            car_state[stack_top(park)][1] = 0;
             car_state[stack_top(tmp_park)][2] += T++;
             stack_pop(tmp_park);
         }
-        stack_push(park, wait.front());  //等待区的车进去停车场
-        wait.pop();
-        car_state[stack_top(park)][0] = T;
+
+        if (!EmptyQueue(wait)) {  //等待区的车进去停车场
+            int o = wait->front->data;
+            stack_push(park, o);
+            car_state[o][0] = T;
+            car_state[o][1] = 0;
+            DeQueue(wait);
+        }
+        cout << endl;
     }
 
     if (car_state[id][1] == 2) {  //在等待区
-        int first_car = wait.front();
-        wait.push(wait.front());
-        wait.pop();
-        while (wait.front() != first_car) {
-            if (wait.front() != id) {
-                wait.push(wait.front());
-                wait.pop();
-            } else {
-                wait.pop();
-            }
+        while (wait->front->data != id) {
+            InQueue(wait, wait->front->data);
+            DeQueue(wait);
         }
+        int o = wait->front->data;  //离开
+        cout << "Car NO. " << o << " in the waiting area has left at time " << T
+             << endl;
+        DeQueue(wait);
     }
+    cout << endl;
 }
 
 int main() {
     int cmd;  // 1代表添加一辆新的车到等待区，2代表查询车辆现在的计费，3代表离开一辆车
     int id = 0, time = 0;
+    park = stack_createStack();      //栈初始化
+    tmp_park = stack_createStack();  //栈初始化
+    wait = InitQueue();              //队列初始化
+
+    memset(car_state, 0, sizeof(car_state));
+    //初始化停车场的大小和单位收费
+    cout << "Please enter the park size: ";
+    cin >> n;
+    cout << "Please enter the park price per unit time: ";
+    cin >> price;
+
+    //界面功能提醒（1次）
+    cout << "Please select the function: " << endl
+         << "1. Add a car" << endl
+         << "2. Query car state" << endl
+         << "3. Leave a car" << endl;
+
     while (scanf("%d", &cmd) != EOF) {
         switch (cmd) {
-            case 1: {
+            case 1: {  //添加车
+                cout << "Command used: Add a car" << endl;
                 cout << "Please input the car id and its enter time: ";
                 cin >> id >> time;
-                T = time;
                 car_in(id, time);
                 cout << endl;
                 break;
             }
-            case 2: {
+            case 2: {  //查询车
+                cout << "Command used: Query car state" << endl;
                 cout << "Please input the car id needed to query and query "
                         "time: ";
                 cin >> id >> time;
@@ -203,13 +304,15 @@ int main() {
                 cout << endl;
                 break;
             }
-            case 3: {
-                cout << "Please input the car id needed to leave the queue: ";
-                cin >> id;
-                car_leave(id);
+            case 3: {  //车离开
+                cout << "Command used: Leave a car" << endl;
+                cout << "Please input the car id needed to leave and "
+                        "its leave time: ";
+                cin >> id >> time;
+                car_leave(id, time);
                 break;
             }
-            default: {
+            default: {  //指令有误
                 cout << "Invalid command, please re-enter:";
                 break;
             }
