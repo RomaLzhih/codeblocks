dbbd389ed0692d38ad8cb3e626c3746d06beb876
ziyang <791191656@qq.com>
2023-02-16 15:38:25 -0800

    get 76

--- KDtree/kdtree.cpp
+++ KDtree/kdtree.cpp
@@ -1,26 +1,33 @@
+#pragma GCC optimize( 3 )
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
+#include <algorithm>
 #include <queue>
 
 #define MAX_DIM 6
+#define MAXN 100000 + 5
+const long double eps = 1e-10;
 
-int K;
-std::priority_queue<double> q;
+int N, Dim, Q, K;
+std::priority_queue<long double> q;
 
 struct kd_node_t
 {
-   double x[MAX_DIM];
+   long double x[MAX_DIM];
    struct kd_node_t *left, *right;
 };
 
-inline double
+struct kd_node_t *root, *found, wp[MAXN];
+
+inline long double
 dist( struct kd_node_t *a, struct kd_node_t *b, int dim )
 {
-   double t, d = 0;
+   long double t, d = 0.0;
    while( dim-- )
    {
       t = a->x[dim] - b->x[dim];
@@ -32,13 +39,12 @@ dist( struct kd_node_t *a, struct kd_node_t *b, int dim )
 inline void
 swap( struct kd_node_t *x, struct kd_node_t *y )
 {
-   double tmp[MAX_DIM];
+   long double tmp[MAX_DIM];
    memcpy( tmp, x->x, sizeof( tmp ) );
    memcpy( x->x, y->x, sizeof( tmp ) );
    memcpy( y->x, tmp, sizeof( tmp ) );
 }
 
-/* see quickselect method */
 struct kd_node_t *
 find_median( struct kd_node_t *start, struct kd_node_t *end, int idx )
 {
@@ -46,7 +52,7 @@ find_median( struct kd_node_t *start, struct kd_node_t *end, int idx )
    if( end == start + 1 ) return start;
 
    struct kd_node_t *p, *store, *md = start + ( end - start ) / 2;
-   double pivot;
+   long double pivot;
    while( 1 )
    {
       pivot = md->x[idx];
@@ -54,7 +60,7 @@ find_median( struct kd_node_t *start, struct kd_node_t *end, int idx )
       swap( md, end - 1 );
       for( store = p = start; p < end; p++ )
       {
-         if( p->x[idx] < pivot )
+         if( p->x[idx] - pivot < -eps )
          {
             if( p != store ) swap( p, store );
             store++;
@@ -62,7 +68,6 @@ find_median( struct kd_node_t *start, struct kd_node_t *end, int idx )
       }
       swap( store, end - 1 );
 
-      /* median has duplicate values */
       if( store->x[idx] == md->x[idx] ) return md;
 
       if( store > md )
@@ -71,6 +76,17 @@ find_median( struct kd_node_t *start, struct kd_node_t *end, int idx )
          start = store;
    }
 }
+
+struct node_cmp
+{
+   node_cmp( size_t index ) : index_( index ) {}
+   bool
+   operator()( const kd_node_t &n1, const kd_node_t &n2 ) const
+   {
+      return n1.x[index_] < n2.x[index_];
+   }
+   size_t index_;
+};
 
 struct kd_node_t *
 make_tree( struct kd_node_t *t, int len, int i, int dim )
@@ -88,117 +104,61 @@ make_tree( struct kd_node_t *t, int len, int i, int dim )
    return n;
 }
 
-/* global variable, so use me */
-int visited;
-
 void
-nearest( struct kd_node_t *root, struct kd_node_t *nd, int i, int dim,
-         struct kd_node_t **best, double *best_dist )
+nearest( struct kd_node_t *root, struct kd_node_t *nd, int i, int dim )
 {
-   double d, dx, dx2;
+   long double d, dx, dx2;
 
    if( !root ) return;
    d = dist( root, nd, dim );
    dx = root->x[i] - nd->x[i];
    dx2 = dx * dx;
+   // printf( "%Lf %Lf\n", sqrt( d ), q.size() ? sqrt( q.top() ) : -1 );
 
-   visited++;
-
-   if( !*best || d < *best_dist )
+   if( q.size() < K || d - q.top() < eps )
    {
-      *best_dist = d;
-      *best = root;
+      q.push( d );
+      if( q.size() > K ) q.pop();
    }
-
-   /* if chance of exact match is high */
-   if( !*best_dist ) return;
 
    if( ++i >= dim ) i = 0;
 
-   nearest( dx > 0 ? root->left : root->right, nd, i, dim, best, best_dist );
-   if( dx2 >= *best_dist ) return;
-   nearest( dx > 0 ? root->right : root->left, nd, i, dim, best, best_dist );
+   nearest( dx > 0 ? root->left : root->right, nd, i, dim );
+   if( dx2 - q.top() >= eps && q.size() >= K ) return;
+   nearest( dx > 0 ? root->right : root->left, nd, i, dim );
 }
 
-#define N 1000000
-#define rand1() ( rand() / (double)RAND_MAX )
-#define rand_pt( v )    \
-   {                    \
-      v.x[0] = rand1(); \
-      v.x[1] = rand1(); \
-      v.x[2] = rand1(); \
-   }
 int
 main( void )
 {
-   int i;
-   struct kd_node_t wp[] = {
-       { { -30256895.771537, -188498875.546657, -934589771.112173 } },
-       { { -257940191.424180, -370268489.887961, -430962755.966749 } },
-       { { -360285956.184535, 162448009.822708, -463563741.924728 } },
-       { { -841003744.119359, 581308628.858605, 471030699.413780 } } };
-   struct kd_node_t testNode = {
-       { -740544115.727460, -562253846.301237, 877933190.625700 } };
-   struct kd_node_t *root, *found, *million;
-   double best_dist;
+#ifndef ONLINE_JUDGE
+   freopen( "../input.txt", "r", stdin );
+#endif
 
-   root = make_tree( wp, sizeof( wp ) / sizeof( wp[1] ), 0, 3 );
+   scanf( "%d %d", &N, &Dim );
+   int i, j;
 
-   visited = 0;
-   found = 0;
-   nearest( root, &testNode, 0, 3, &found, &best_dist );
-
-   q.push( 0.00001 );
-   q.push( 0.00002 );
-   printf( "%f\n", q.top() );
-
-   printf(
-       ">> WP tree\nsearching for (%f, %f, %f)\n"
-       "found (%f, %f, %f) dist %f\nseen %d nodes\n\n",
-       testNode.x[0], testNode.x[1], testNode.x[2], found->x[0], found->x[1],
-       found->x[2], sqrt( best_dist ), visited );
-
-   million = (struct kd_node_t *)calloc( N, sizeof( struct kd_node_t ) );
-   srand( time( 0 ) );
-   for( i = 0; i < N; i++ ) rand_pt( million[i] );
-
-   root = make_tree( million, N, 0, 3 );
-   rand_pt( testNode );
-
-   visited = 0;
-   found = 0;
-   nearest( root, &testNode, 0, 3, &found, &best_dist );
-
-   printf(
-       ">> Million tree\nsearching for (%g, %g, %g)\n"
-       "found (%g, %g, %g) dist %g\nseen %d nodes\n",
-       testNode.x[0], testNode.x[1], testNode.x[2], found->x[0], found->x[1],
-       found->x[2], sqrt( best_dist ), visited );
-
-   /* search many random points in million tree to see average behavior.
-      tree size vs avg nodes visited:
-      10      ~  7
-      100     ~ 16.5
-      1000        ~ 25.5
-      10000       ~ 32.8
-      100000      ~ 38.3
-      1000000     ~ 42.6
-      10000000    ~ 46.7              */
-   int sum = 0, test_runs = 100000;
-   for( i = 0; i < test_runs; i++ )
+   for( i = 0; i < N; i++ )
    {
-      found = 0;
-      visited = 0;
-      rand_pt( testNode );
-      nearest( root, &testNode, 0, 3, &found, &best_dist );
-      sum += visited;
+      for( j = 0; j < Dim; j++ )
+      {
+         scanf( "%Lf", &wp[i].x[j] );
+      }
    }
-   printf(
-       "\n>> Million tree\n"
-       "visited %d nodes for %d random findings (%f per lookup)\n",
-       sum, test_runs, sum / (double)test_runs );
 
-   // free(million);
+   root = make_tree( wp, N, 0, Dim );
+
+   scanf( "%d", &Q );
+   struct kd_node_t z;
+   while( Q-- )
+   {
+      scanf( "%d", &K );
+      for( j = 0; j < Dim; j++ ) scanf( "%Lf", &z.x[j] );
+      while( !q.empty() ) q.pop();
+      nearest( root, &z, 0, Dim );
+      // printf( "%ld\n", q.size() );
+      printf( "%.8Lf\n", sqrt( q.top() ) );
+   }
 
    return 0;
 }
